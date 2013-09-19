76884ab3159b071b2d2fe7e950f436462bd28ece
Alexander Andreev <cinic.rus@gmail.com>
2013-09-19 04:12:19 +0400

    Добавлена возможност логиниться и регистрироваться

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-// require jquery_ujs
-// require jquery_nested_form
 
 //= require jquery-1.9.1.min
+//= require jquery_ujs
+//= require jquery_nested_form
 //= require lazyload
 //= require jRespond
 //= require spin
