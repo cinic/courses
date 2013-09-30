2ee0277a0b08fe332d5fe152cf32f869eb8ca211
Aleksandr <andreev-a@open.ru>
2013-09-30 13:38:52 +0400

    Добавил правильный путь до account.js & account.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ Courses::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( account.js, account.css )
+  config.assets.initialize_on_precompile = false
+  config.assets.precompile += ['account.js', 'account.css']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
